8528bfa30eba93e96f552359e8bf5ae61820ddd7
blackapple113 <t-schueller@gmx.de>
2017-10-26 14:55:13 +0200

    SchrankenstuerungTester hinzugefügt <Package tschue2s>

---
+++ Parkhaus/src/tschue2s/SchrankensteuerungTester.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author tschue2s
+ *
+ */
+package tschue2s;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SchrankensteuerungTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
