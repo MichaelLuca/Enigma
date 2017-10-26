a0523704f1fcbcf0f0332ae96e8eefa731c4909d
blackapple113 <t-schueller@gmx.de>
2017-10-26 16:03:13 +0200

    SchrankensteuerungTester hinzugefügt <Package tschue2s>

--- Parkhaus/src/tschue2s/SchrankensteuerungTester.java
+++ Parkhaus/src/tschue2s/SchrankensteuerungTester.java
@@ -6,14 +6,33 @@
 package tschue2s;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 import org.junit.Test;
 
+import java.time.*;
+
 public class SchrankensteuerungTester {
 
+	@Before
+	public void setUp() {
+		Auto a1 = new Auto();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void time_AutoEnterParkhaus_returnsTrue() {
+		LocalDateTime now = LocalDateTime.now();
+		assertEquals(now, a1.enterParkhaus());
+	}
+	
+	@Test
+	public void place_AutoPlatz_returnsTrue() {
+		assertEquals(1, a1.place());
+	}
+	
+	@Test
+	public void price_ParkingPrice_returnsTrue() {
+		assertEquals("1,89", a1.leaveParkhaus());
 	}
 
 }
